96f03d26cd6b2afe4f9a7546a2cba252309ea34a
Daniel McFadden <dmcfaddengalway@gmail.com>
2020-01-27 22:38:52 +0000

    implemented stuent

---
+++ Student.java
@@ -0,0 +1,28 @@
+public class Student {
+
+  // Member Variables
+  private String password;
+  private int id;
+
+  // Constructor
+  public Student(String pw, int idNo) {
+    this.password = pw;
+    this.id = idNo;
+  }
+
+  // Methods
+
+  // Accessor Methods
+
+    // Setters
+
+    // Getters
+    public String getPassword() {
+      return password;
+    }
+
+    public int getId() {
+      return id;
+    }
+
+}
