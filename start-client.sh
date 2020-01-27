04d74b41be9bf6871a9a6b4b52876d093a61893c
Daniel McFadden <dmcfaddengalway@gmail.com>
2020-01-27 22:49:21 +0000

    update scripts

--- start-client.sh
+++ start-client.sh
@@ -1,1 +1,1 @@
-java -cp /Users/des/rmidemo/compute.jar:. -Djava.security.policy=client.policy client.ComputePi 20345 45
+java -cp /Users/dannymcfadden/rmidemo/compute.jar:. -Djava.security.policy=client.policy client.ComputePi 20345 45
--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,1 @@
-java -cp /Users/des/rmidemo/compute.jar:. -Djava.security.policy=server.policy engine.ComputeEngine 20345
+java -cp /Users/dannymcfadden/rmidemo/compute.jar:. -Djava.security.policy=server.policy engine.ComputeEngine 20345
